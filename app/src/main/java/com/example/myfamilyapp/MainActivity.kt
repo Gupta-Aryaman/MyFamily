4ed6a9713d911280d47f5a63b4c0c270482d6682
Aryaman <aryaman28112002@gmail.com>
2024-09-30 00:41:08 +0530

    create guard and home fragments

--- app/src/main/java/com/example/myfamilyapp/MainActivity.kt
+++ app/src/main/java/com/example/myfamilyapp/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.myfamilyapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+        inflateFragment(HomeFragment.newInstance())
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+        bottomBar.setOnItemSelectedListener {
+            // 'it' is the object that this listener gives us
+            if (it.itemId == R.id.nav_guard) {
+                inflateFragment(GuardFragment.newInstance())
+            } else if (it.itemId == R.id.nav_home) {
+                inflateFragment(HomeFragment.newInstance())
+            }
+            true
+        }
+    }
+
+    private fun inflateFragment(newInstance: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, newInstance)
+        transaction.commit()
     }
 }
